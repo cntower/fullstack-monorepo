c2b06c47704075a1a0dd9bef69dea8e095b234a1
Stanislav Baturin <stasbaturin78@gmail.com>
2019-05-11 18:46:26 +0300

    add dotenv

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,16 +2,15 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  **/
-
+import 'dotenv/config'
 import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app/app.module";
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = "api";
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.port || 3333;
+  const port = process.env.API_PORT || 3333;
   await app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/${globalPrefix}`);
   });
